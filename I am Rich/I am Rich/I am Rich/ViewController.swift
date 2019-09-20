166077eea76566803af87e245a28448310214183
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-09-20 13:38:33 -0700

    Eh! First page lol

---
+++ I am Rich/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Tanish arora on 9/19/19.
+//  Copyright © 2019 Tanish arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
